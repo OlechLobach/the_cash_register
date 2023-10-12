0e1a7178e57db8f068c6053d197c9912f096abff
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-10-13 00:42:34 +0300

    inital commit

--- main.py
+++ main.py
@@ -2,36 +2,30 @@ class Product:
     def __init__(self, name, price):
         self.name = name
         self.price = price
-
 class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
         self.cart = []
-
 class Admin:
     def __init__(self, username, password):
         self.username = username
         self.password = password
         self.products = []
-
 def main():
     admin = Admin("Admin", "Admin")
     users = []
-
     while True:
         print("\n1. Зареєструватись")
         print("2. Увійти як користувач")
         print("3. Увійти як адміністратор")
         print("4. Вийти")
         choice = input("Оберіть опцію: ")
-
         if choice == "1":
             username = input("Введіть ім'я користувача: ")
             password = input("Введіть пароль: ")
             users.append(User(username, password))
             print("Користувач зареєстрований.")
-
         elif choice == "2":
             username = input("Введіть ім'я користувача: ")
             password = input("Введіть пароль: ")
@@ -40,7 +34,6 @@ def main():
                 user_menu(user, admin.products)
             else:
                 print("Невірне ім'я користувача або пароль.")
-
         elif choice == "3":
             username = input("Введіть ім'я адміністратора: ")
             password = input("Введіть пароль адміністратора: ")
@@ -48,19 +41,15 @@ def main():
                 admin_menu(admin)
             else:
                 print("Невірне ім'я адміністратора або пароль.")
-
         elif choice == "4":
             break
-
         else:
             print("Невірний вибір. Спробуйте ще раз.")
-
 def find_user(username, password, users):
     for user in users:
         if user.username == username and user.password == password:
             return user
     return None
-
 def user_menu(user, products):
     while True:
         print("\n1. Показати список товарів")
@@ -69,31 +58,23 @@ def user_menu(user, products):
         print("4. Закінчити покупку")
         print("5. Вийти")
         choice = input("Оберіть опцію: ")
-
         if choice == "1":
             show_products(products)
-
         elif choice == "2":
             add_to_cart(user, products)
-
         elif choice == "3":
             show_cart(user)
-
         elif choice == "4":
             checkout(user)
             break
-
         elif choice == "5":
             break
-
         else:
             print("Невірний вибір. Спробуйте ще раз.")
-
 def show_products(products):
     print("\nСписок товарів:")
     for i, product in enumerate(products, 1):
         print(f"{i}. {product.name} - {product.price} грн")
-
 def add_to_cart(user, products):
     show_products(products)
     choice = input("Виберіть товар для додавання у кошик (введіть номер товару): ")
@@ -106,20 +87,17 @@ def add_to_cart(user, products):
             print("Невірний номер товару.")
     except ValueError:
         print("Введіть коректний номер товару.")
-
 def show_cart(user):
     total_price = sum(product.price for product in user.cart)
     print("\nКошик:")
     for i, product in enumerate(user.cart, 1):
         print(f"{i}. {product.name} - {product.price} грн")
     print(f"Загальна сума до оплати: {total_price} грн")
-
 def checkout(user):
     total_price = sum(product.price for product in user.cart)
     print(f"Загальна сума до оплати: {total_price} грн")
     user.cart.clear()
     print("Покупка успішно завершена.")
-
 def admin_menu(admin):
     while True:
         print("\n1. Додати товар")
@@ -127,28 +105,21 @@ def admin_menu(admin):
         print("3. Змінити інформацію про товар")
         print("4. Вийти")
         choice = input("Оберіть опцію: ")
-
         if choice == "1":
             add_product(admin.products)
-
         elif choice == "2":
             delete_product(admin.products)
-
         elif choice == "3":
             update_product(admin.products)
-
         elif choice == "4":
             break
-
         else:
             print("Невірний вибір. Спробуйте ще раз.")
-
 def add_product(products):
     name = input("Введіть назву товару: ")
     price = float(input("Введіть ціну товару: "))
     products.append(Product(name, price))
     print("Товар додано.")
-
 def delete_product(products):
     show_products(products)
     choice = input("Виберіть товар для видалення (введіть номер товару): ")
@@ -161,7 +132,6 @@ def delete_product(products):
             print("Невірний номер товару.")
     except ValueError:
         print("Введіть коректний номер товару.")
-
 def update_product(products):
     show_products(products)
     choice = input("Виберіть товар для зміни (введіть номер товару): ")
@@ -178,6 +148,5 @@ def update_product(products):
             print("Невірний номер товару.")
     except ValueError:
         print("Введіть коректний номер товару.")
-
 if __name__ == "__main__":
     main()
